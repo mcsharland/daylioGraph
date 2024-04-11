5a59c49020eca1dce96e5a2f2128a316bff8a460
mcsharland <mcsharland@mail.csuchico.edu>
2024-04-11 03:35:45 -0700

    Refactor and improved colors

--- daylio.py
+++ daylio.py
@@ -1,64 +1,103 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
+import numpy as np
+from matplotlib.ticker import (MultipleLocator, NullFormatter)
 import sys
 
 daylio_csv = sys.argv[1]
 data = pd.read_csv(daylio_csv)
-
-mood_mapping = {'awful': 1, 'bad': 2, 'meh': 3, 'good': 4, 'rad': 5}
+mood_mapping = {'awful': 0.5, 'bad': 1.5, 'meh': 2.5, 'good': 3.5, 'rad': 4.5}
 mood_colors = {'awful': 'red', 'bad': 'orange', 'meh': 'deepskyblue', 'good': 'lime', 'rad': 'turquoise'}
+mood_boundaries = [1, 2, 3, 4]
 
 data['mood_value'] = data['mood'].map(mood_mapping)
 data['full_date'] = pd.to_datetime(data['full_date'])
 data_sorted = data.sort_values(by='full_date')
 
 MIN_PIXEL_DIST = 20
-
-dpi = 100
-
-default_window_size = 20
-
-num_points = len(data_sorted)
-
-pixel_distance = default_window_size * dpi / (num_points - 1)
+DPI = 100
+DEFAULT_WINDOW_SIZE = 20
+NUM_POINTS = len(data_sorted)
+pixel_distance = DEFAULT_WINDOW_SIZE * DPI / (NUM_POINTS - 1)
 
 if pixel_distance < MIN_PIXEL_DIST:
     scale_factor = MIN_PIXEL_DIST / pixel_distance
     pixel_distance = MIN_PIXEL_DIST
-    window_size = default_window_size * scale_factor
+    window_size = DEFAULT_WINDOW_SIZE * scale_factor
 else:
-    window_size = default_window_size
+    window_size = DEFAULT_WINDOW_SIZE
 
 base_height = 6
+fig, ax = plt.subplots(figsize=(window_size, base_height), dpi=DPI)
 
-plt.figure(figsize=(window_size, base_height), dpi=dpi)
-
+#color code graph
 for i in range(len(data_sorted) - 1):
-    start_mood = data_sorted['mood'].iloc[i]
-    end_mood = data_sorted['mood'].iloc[i + 1]
-    color = mood_colors[start_mood]
-    plt.plot(data_sorted['full_date'].iloc[i:i+2], data_sorted['mood_value'].iloc[i:i+2], marker='o', linestyle='-', color=color, linewidth=2)
+    start_mood = data_sorted['mood_value'].iloc[i]
+    end_mood = data_sorted['mood_value'].iloc[i + 1]
+    x1, y1 = mdates.date2num(data_sorted['full_date'].iloc[i]), start_mood
+    x2, y2 = mdates.date2num(data_sorted['full_date'].iloc[i + 1]), end_mood
 
-    if i == len(data_sorted) - 2:
-        plt.plot(data_sorted['full_date'].iloc[-1], data_sorted['mood_value'].iloc[-1], marker='o', color=mood_colors[end_mood])
+    if y1 != y2:
+        m = (y2 - y1) / (x2 - x1)
+        intersection_points = [(x1, y1)]
 
-plt.title(f'Average mood of {round(data["mood_value"].mean(),2)} over {len(data)} days', fontsize=16)
-plt.xlabel('Date', fontsize=12)
-plt.ylabel('Mood', fontsize=12)
+        for boundary in mood_boundaries:
+            if (y1 < boundary < y2) or (y2 < boundary < y1):
+                x_intersection = x1 + (boundary - y1) / m
+                intersection_points.append((x_intersection, boundary))
 
-labels=['Awful', 'Bad', 'Meh', 'Good', 'Rad']
-label_colors = [mood_colors[mood.lower()] for mood in labels]
-plt.yticks(ticks=[1, 2, 3, 4, 5], labels=labels)
-for i, label in enumerate(plt.gca().get_yticklabels()):
-    label.set_color(label_colors[i])
+        intersection_points.append((x2, y2))
+        intersection_points.sort(key=lambda x: x[0])
 
-plt.grid(axis='y', linestyle='--')
+        for j in range(len(intersection_points) - 1):
+            x_start, y_start = intersection_points[j]
+            x_end, y_end = intersection_points[j + 1]
+            mood_range = (int(min(y_start, y_end)), int(max(y_start, y_end)))
+            color = mood_colors[list(mood_mapping.keys())[mood_range[0]]]
+            ax.plot([mdates.num2date(x_start), mdates.num2date(x_end)], [y_start, y_end], linestyle='-', color=color, linewidth=2)
+    else:
+        color = mood_colors[data_sorted['mood'].iloc[i]]
+        ax.plot([data_sorted['full_date'].iloc[i], data_sorted['full_date'].iloc[i + 1]], [y1, y2], linestyle='-', color=color, linewidth=2)
 
-plt.gca().xaxis.set_major_locator(mdates.MonthLocator())
-plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m'))
-plt.gcf().autofmt_xdate()
+#color points
+for mood, color in mood_colors.items():
+    mask = data_sorted['mood'] == mood
+    ax.plot(data_sorted.loc[mask, 'full_date'], data_sorted.loc[mask, 'mood_value'], marker='o', linestyle='None', color=color)
+
+ax.set_title(f'Average mood of {round(data["mood_value"].mean(),2)} over {len(data)} days', fontsize=16)
+ax.set_xlabel('Date', fontsize=12)
+ax.set_ylabel('Mood', fontsize=12, labelpad=25)
+
+ax.grid(axis='y', linestyle='--')
+
+ax.yaxis.set_major_locator(MultipleLocator(1))
+ax.yaxis.set_minor_locator(MultipleLocator(0.5))
+major_ticks = np.arange(0, 6, 1)
+ax.set_yticks(major_ticks)
+ax.yaxis.set_major_formatter(NullFormatter())
+minor_ticks = np.arange(0.5, 5.5, 1)
+ax.set_yticks(minor_ticks, minor=True)
+ax.tick_params(axis='y', which='both', length=0)
+
+for mood, value in mood_mapping.items():
+    ax.text(0, value, mood.capitalize() + ' ', transform=ax.get_yaxis_transform(), ha='right', va='center', color=mood_colors[mood])
+
+ax.xaxis.set_major_locator(mdates.MonthLocator())
+ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m'))
+
+min_date = data_sorted['full_date'].min()
+max_date = data_sorted['full_date'].max()
+graph_padding = 25 
+date_range = max_date - min_date
+pixel_width = fig.get_figwidth() * DPI
+date_padding = date_range * (graph_padding / pixel_width)
+ax.set_xlim(min_date - date_padding, max_date + date_padding)
+
+fig.autofmt_xdate()
+
+plt.subplots_adjust(bottom=0.15, top=0.9)
 plt.tight_layout()
 
-plt.savefig('graph.jpg', dpi=dpi, format='jpg')
-# plt.show() # Uncomment to open the graph after running the program
+plt.savefig('graph.jpg', dpi=DPI, format='jpg')
+# plt.show() # Uncomment to display the graph
